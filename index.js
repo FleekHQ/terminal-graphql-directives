d6007d2023f2e8398b0874c26007b4fcaca5d74b
dmolina79 <doug.molinam@gmail.com>
2019-04-03 16:50:29 -0600

    fix

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const { SchemaDirectiveVisitor } = require('graphql-tools');
+/* eslint-disable import/prefer-default-export */
+import requiresAuth from './src/requires-auth';
 
-class TemplateDirective extends SchemaDirectiveVisitor {
-  //
-  // Check this article for implementation details:
-  // https://dev-blog.apollodata.com/reusable-graphql-schema-directives-131fb3a177d1
-  //
-  // also check out the SchemaDirective docs from graphql-tools
-  // https://www.apollographql.com/docs/graphql-tools/schema-directives.html
-  //
-}
-
-module.exports = TemplateDirective;
+export { requiresAuth };
